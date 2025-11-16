7a812d22d8a6a3e6da21b961161fa2086bd2c787
Sujay <snshah93@gmail.com>
2025-11-16 10:52:01 +0530

    - added camera component UI in properties, added scene camera - added Sprite component UI

--- Engine/src/Engine/Scene/SceneCamera.cpp
+++ Engine/src/Engine/Scene/SceneCamera.cpp
@@ -7,14 +7,25 @@ namespace Engine {
 
 	SceneCamera::SceneCamera()
 	{
+		m_type = CameraType::Orthographic;
 		RecalculateProjection();
 	}
 
 	void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
 	{
+		m_type = CameraType::Orthographic;
 		m_OrthographicSize = size;
 		m_OrthographicNear = nearClip;
 		m_OrthographicFar = farClip;
+		RecalculateProjection();
+	}
+
+	void SceneCamera::SetPerspective(float verticalFOV, float nearClip, float farClip)
+	{
+		m_type = CameraType::Perspective; 
+		m_PerspectiveFOV = verticalFOV;
+		m_PerspectiveNear = nearClip;
+		m_PerspectiveFar = farClip;
 		RecalculateProjection();
 	}
 
@@ -26,13 +37,20 @@ namespace Engine {
 
 	void SceneCamera::RecalculateProjection()
 	{
-		float orthoLeft = -m_OrthographicSize * m_AspectRatio * 0.5f;
-		float orthoRight = m_OrthographicSize * m_AspectRatio * 0.5f;
-		float orthoBottom = -m_OrthographicSize * 0.5f;
-		float orthoTop = m_OrthographicSize * 0.5f;
+		if (IsPerspective())
+		{
+			m_ProjectionMatrix = glm::perspective(m_PerspectiveFOV, m_AspectRatio, m_PerspectiveNear, m_PerspectiveFar);
+		}
+		else
+		{
+			float orthoLeft = -m_OrthographicSize * m_AspectRatio * 0.5f;
+			float orthoRight = m_OrthographicSize * m_AspectRatio * 0.5f;
+			float orthoBottom = -m_OrthographicSize * 0.5f;
+			float orthoTop = m_OrthographicSize * 0.5f;
 
-		m_ProjectionMatrix = glm::ortho(orthoLeft, orthoRight,
-			orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+			m_ProjectionMatrix = glm::ortho(orthoLeft, orthoRight,
+				orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+		}	
 	}
 
 }
